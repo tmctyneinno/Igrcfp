b1af340778b4ff19c2facb4462460c459e41e67c
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 17:55:36 +0100

    Updated Changes

--- resources/js/Pages/components/Footer.jsx
+++ resources/js/Pages/components/Footer.jsx
@@ -251,14 +251,13 @@ export default function Footer() {
                                     </address>
                                 </div>
                             </li>
-                            <li className="flex items-start gap-4">
+                            {/* <li className="flex items-start gap-4">
                                 <div className="w-10 h-10 rounded-lg bg-blue-900/30 flex items-center justify-center flex-shrink-0">
-                                    {/* Phone Icon */}
-                                    <svg className="w-5 h-5 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                     <svg className="w-5 h-5 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                     </svg>
                                 </div>
-                                {/* <div>
+                                <div>
                                     <span className="block text-sm text-gray-500 mb-1">Phone</span>
                                     <a 
                                         href="tel:+442071234567" 
@@ -266,8 +265,8 @@ export default function Footer() {
                                     >
                                         +44 (0)20 7123 4567
                                     </a>
-                                </div> */}
-                            </li>
+                                </div>
+                            </li> */}
                         </ul>
 
                        
