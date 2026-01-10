76348d91779de7c71a3b6c1d0d36867a038dc2de
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 18:59:02 +0100

    Updated Changes

--- resources/js/Pages/components/SplitHeroSlider.jsx
+++ resources/js/Pages/components/SplitHeroSlider.jsx
@@ -82,7 +82,7 @@ export default function SplitHeroSlider({ auth }) {
             onMouseEnter={stopAutoSlide}
             onMouseLeave={startAutoSlide}
         >
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 lg:py-20">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 lg:py-20">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
                     
                     {/* Left Column - Text Slider - SIMPLE FIXED VERSION */}
