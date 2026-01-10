914de4eacebcff8160325a81f96440ea754a0ca7
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 01:52:24 +0100

    Updated Changes

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,10 +3,8 @@ import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            
-
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+        <div className="">
+            <div className="">
                 {children}
             </div>
         </div>
