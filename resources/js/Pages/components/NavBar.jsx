84d7e7b2efa7a56ef1e969377d1ed0371d536769
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 18:53:02 +0100

    Updated Changes

--- resources/js/Pages/components/NavBar.jsx
+++ resources/js/Pages/components/NavBar.jsx
@@ -1,7 +1,7 @@
 
 import { Link } from "@inertiajs/react";
 
-export default function BecomeMember() {
+export default function NavBar() {
     return (
        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16">
