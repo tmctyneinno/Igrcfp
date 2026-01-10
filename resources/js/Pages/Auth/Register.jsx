57d934d7b7fef11223e6a1e94144ece5d82f413d
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 03:26:27 +0100

    Updated Changes

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -12,7 +12,7 @@ export default function Register() {
         email: '',
         password: '',
         password_confirmation: '',
-        role: 'student',
+        role: 'learner',
         phone: '',
         linkedin: '',
         date_of_birth: '',
