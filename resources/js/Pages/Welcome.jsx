a1980d9be484edc115bd81cb706fe816ee08f569
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 03:50:26 +0100

    Updated Changes

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -17,8 +17,8 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
             <Head title="Home" />
             <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 
-                <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-                    <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                <div id="smooth-wrapper">
+                    <div id="smooth-content">
                         <header className="header-area header-three fixed-top">
                             <div className="flex lg:col-start-2 lg:justify-center">
                                 <svg
