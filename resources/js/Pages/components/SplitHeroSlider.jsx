dcf969fd307125e330682db86d088d1974bbddf4
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 11:11:07 +0100

    Updated Changes

--- resources/js/Pages/components/SplitHeroSlider.jsx
+++ resources/js/Pages/components/SplitHeroSlider.jsx
@@ -12,7 +12,7 @@ export default function SplitHeroSlider({ auth }) {
             highlighted: "Tutors",
             description: "The Institute of GRC & Financial Crime Prevention (IGRCFP) is a global professional  body dedicated to advancing excellence in governance, risk management,  compliance, and financial crime prevention.",
             image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200&q=80",
-            ctaPrimary: "Start Learning Free",
+            ctaPrimary: "Join the",
             ctaSecondary: "Browse Courses"
         },
         {
