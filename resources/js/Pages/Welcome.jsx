dd6a13be68143c6c661f607176f63ba25b3b855e
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 11:12:07 +0100

    Updated Changes

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -229,7 +229,7 @@ export default function Welcome({ auth }) {
                                         href={route('login')}
                                         className="bg-blue-950 text-white px-6 py-2 rounded-lg font-medium hover:bg-blue-700 transition duration-300 shadow-md hover:shadow-lg"
                                     >
-                                        Join the Insitute
+                                        Join the Institute
                                     </Link>
                                 </>
                             )}
