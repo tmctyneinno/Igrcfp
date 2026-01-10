2a42e7934e31c8547524504fcd4cd5fed516aaf0
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 16:04:35 +0100

    Updated Changes

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -444,6 +444,10 @@ export default function Welcome({ auth, courses }) {
             <section className="bg-white py-24 overflow-hidden" data-aos="zoom-in" data-aos-duration="1200">
                 <GlobalEvents />
             </section>
+
+            <section className="bg-white py-24 overflow-hidden" data-aos="zoom-in" data-aos-duration="1200">
+                <GlobalEvents />
+            </section>
             
                 {/* CTA Section with AOS zoom effect */}
                 <section 
