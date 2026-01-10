73503051782d019f06c2c7039c17753410329dea
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 01:33:24 +0100

    Updated Changes

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -24,7 +24,7 @@ export default function Login({ status, canResetPassword }) {
     return (
         <GuestLayout>
             <Head title="Log in" />
-
+ 
             {status && (
                 <div className="mb-4 text-sm font-medium text-green-600">
                     {status}
