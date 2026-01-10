965d2663e0ff72a01c73222f655a350440597a5f
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-10 18:02:37 +0100

    Updated Changes

--- resources/js/Pages/components/Footer.jsx
+++ resources/js/Pages/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
     return (    
         <footer className="">
             {/* Decorative elements */}
-          \
+          
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 lg:py-20 relative">
                
                 {/* Main Footer Content */}
