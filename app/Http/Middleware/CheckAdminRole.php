712730ab475daae7339a17ff117981202c210901
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 10:53:06 +0100

    Updated Changes

--- app/Http/Middleware/CheckAdminRole.php
+++ app/Http/Middleware/CheckAdminRole.php
@@ -4,13 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckAdminRole
 {
-    public function handle(Request $request, Closure $next, ...$roles)
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $admin = $request->user('admin');
+        $admin = Auth::guard('admin')->user();
         
         if (!$admin) {
             return redirect()->route('admin.login');
