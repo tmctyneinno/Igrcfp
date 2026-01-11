3ae1896521c9aee9ab6cf900e76ad557386421fe
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 22:14:01 +0100

    Updated Changes

--- resources/js/Pages/Events/Register.jsx
+++ resources/js/Pages/Events/Register.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Head } from '@inertiajs/react';
 import { format, parseISO } from 'date-fns';
 import axios from 'axios';
