a899984f610b759b7ef48d401251658de94abf58
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 07:21:04 +0100

    Updated Changes

--- resources/js/Pages/About/CoreServices.jsx
+++ resources/js/Pages/About/CoreServices.jsx
@@ -44,7 +44,7 @@ export default function CoreServices() {
         <section className="py-20 bg-white">
             <div className="max-w-6xl mx-auto px-4 text-center">
                 <h1 className="text-3xl font-bold text-gray-900 mb-8">Our Core Services</h1>
-                <p className='mb-12 text-md'>
+                <p className='mb-12 text-lg'>
                     At IGRCFP, we empower professionals and institutions through education, membership, research, and global events. Our core services are designed to strengthen governance, compliance, and financial crime prevention worldwide
                 </p>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
