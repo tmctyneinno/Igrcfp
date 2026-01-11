a38e65b2875879a3f4e1f3c5090369230aa77f30
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 08:39:05 +0100

    Updated Changes

--- resources/js/Pages/Membership/Index.jsx
+++ resources/js/Pages/Membership/Index.jsx
@@ -7,7 +7,7 @@ export default function Membership({ auth, title, description }) {
         <GuestLayout auth={auth}>
             <Head title={title} />
             {/* Hero Section */}
-            <section className="w-full bg-gradient-to-r from-blue-200 via-white to-blue-200 py-28">
+            <section className="w-full bg-gradient-to-r from-blue-200 via-white to-blue-200 py-20">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                    <div className="text-center">
                         <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-6">
