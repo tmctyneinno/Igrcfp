e3f7208af396999fc4f4d4bba3d9e2ad80639448
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 07:29:05 +0100

    Updated Changes

---
+++ resources/js/Pages/About/OurStructure/Index.jsx
@@ -0,0 +1,42 @@
+import { Head, Link } from '@inertiajs/react';
+import GuestLayout from '@/Layouts/GuestLayout';
+import WhoAreWe from "@/Pages/components/WhoAreWe";
+import OurValues from '@/Pages/About/OurValues';
+import CoreServices from '@/Pages/About/CoreServices';
+import PartnersSlider from '@/Pages/About/PartnersSlider';
+import VisionMissionSlider from '@/Pages/About/VisionMissionSlider';
+
+export default function AboutIndex({ auth, title, description }) {
+    return (
+        <GuestLayout auth={auth}>
+            <Head title={title} />
+            {/* Hero Section */}
+            <section className="w-full bg-gradient-to-r from-blue-200 via-white to-blue-200 py-28">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                   <div className="text-center">
+                        <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-6">
+                            {title}
+                        </h1>
+                        <p className="text-xl text-gray-600 max-w-3xl mx-auto">
+                            The Institute of Governance, Risk, Compliance & Financial Crime Prevention 
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            <PartnersSlider />
+
+            <section className="bg-white py-24 overflow-hidden">
+                <WhoAreWe auth={auth} />
+            </section>
+
+            <VisionMissionSlider />
+            
+            <OurValues />
+
+            <CoreServices />
+
+           
+        </GuestLayout>
+    );
+}
