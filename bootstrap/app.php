85d7769db5298c17d766db7ebe98174e3f95a658
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 10:51:05 +0100

    Updated Changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,8 +9,13 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )
+    ) 
     ->withMiddleware(function (Middleware $middleware) {
+         // Add your middleware aliases here
+        $middleware->alias([
+            'admin.role' => \App\Http\Middleware\CheckAdminRole::class,
+            'auth.admin' => \App\Http\Middleware\AuthenticateAdmin::class, // Optional: create this too
+        ]);
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
