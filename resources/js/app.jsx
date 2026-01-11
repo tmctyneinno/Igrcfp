97b73076e3391409e37b0762811e832a4bef2170
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 22:02:01 +0100

    Updated Changes

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,6 @@
 import '../css/app.css';
 import './bootstrap';
-
+import React from 'react';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
