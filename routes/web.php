c18553de60832898c8171579d2f652b0aba7610d
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 21:51:00 +0100

    Updated Changes

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,7 @@ Route::middleware('auth')->group(function () {
 Auth::routes();
 
 require __DIR__.'/admin/admins.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
