4474a0491a7061c864ca542056f49e6eea2b5230
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 09:02:05 +0100

    Updated Changes

--- resources/js/Pages/Membership/Index.jsx
+++ resources/js/Pages/Membership/Index.jsx
@@ -2,6 +2,7 @@ import { Head, Link } from '@inertiajs/react';
 import GuestLayout from '@/Layouts/GuestLayout';
 import MembershipCTA from '@/Pages/Membership/MembershipCTA';
 import MembershipOptions from "@/Pages/components/MembershipOptions";
+import JoinIGRCFP from "@/Pages/components/MembershipTable";
 import JoinIGRCFP from "@/Pages/components/JoinIGRCFP";
 
 export default function Membership({ auth, title, description }) {
@@ -25,6 +26,7 @@ export default function Membership({ auth, title, description }) {
             <MembershipCTA />
             <JoinIGRCFP />
             <MembershipOptions />
+            <MembershipTable/>
            
         </GuestLayout>
     );
