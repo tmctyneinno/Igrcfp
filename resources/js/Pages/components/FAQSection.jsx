650698090d17b8ec8e9b35f53d4ae7162c969863
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 22:11:01 +0100

    Updated Changes

--- resources/js/Pages/components/FAQSection.jsx
+++ resources/js/Pages/components/FAQSection.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronDown } from "lucide-react";
 import { fadeLeft } from "@/utils/motionPresets";
+
 
 const faqs = [
     {
