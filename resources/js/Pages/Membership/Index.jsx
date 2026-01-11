dfe9e4dd7005eba57f3f0754da8b299d3948ac8b
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 08:28:05 +0100

    Updated Changes

--- resources/js/Pages/Membership/Index.jsx
+++ resources/js/Pages/Membership/Index.jsx
@@ -20,7 +20,7 @@ export default function Membership({ auth, title, description }) {
                 </div>
             </section>
 
-            <BoardOfTrustees />
+            <MembershipCTA />
  
            
         </GuestLayout>
