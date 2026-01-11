9c848e75f19c4c7958245c1380f5b6b1e23beeb9
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 11:15:06 +0100

    Updated Changes

--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/admin/auth.php';
-require __DIR__.'/admin.php';
+require __DIR__.'/admin/admins.php';
