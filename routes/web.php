054025de76434be198485f9328bf247ccb9b4d63
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 21:39:00 +0100

    Updated Changes

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/register', function () { return inertia('Auth/Register');})->name('register');
     Route::get('/login', function () { return inertia('Auth/Login');})->name('login');
     
-    Route::post('/register', [LoginController::class, 'register'])->name('register.post');
+    Route::post('/register', [RegistrationController::class, 'register'])->name('register.post');
     Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 });
 
