ffa92bc113f1311f7efae0c0ebcccaf526197c5f
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 22:17:01 +0100

    Updated Changes

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
