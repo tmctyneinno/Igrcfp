3b10ab7d27c6986f6acec512b3a17fe93b11e443
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-11 18:43:05 +0100

    Updated Changes

--- resources/js/Pages/Events/Index.jsx
+++ resources/js/Pages/Events/Index.jsx
@@ -128,7 +128,7 @@ export default function Events({ auth, title, description, events }) {
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                                     </svg>
                                                     <span className="font-medium">
-                                                    {event.start_date ? (event.start_date) : 'Date TBA'}
+                                                    {event.start_date ? formatDate(event.start_date) : 'Date TBA'}
                                                 </span>
                                                 </div>
 
