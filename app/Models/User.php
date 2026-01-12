1c0162f7a74008c91bad16a2c116bc06b405dbed
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-12 18:57:53 +0100

    admin2

--- app/Models/User.php
+++ app/Models/User.php
@@ -88,7 +88,6 @@ class User extends Authenticatable
         'sms_notifications' => 'boolean',
         'newsletter_subscription' => 'boolean',
         'marketing_emails' => 'boolean',
-        'deleted_at' => 'datetime',
     ];
 
     /**
