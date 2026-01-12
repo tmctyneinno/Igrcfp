711d8ae1a283f8f6451ad0c3e195d53f8d8e8415
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-12 08:09:23 +0100

    Updated Changes

--- resources/js/Pages/Contact/Index.jsx
+++ resources/js/Pages/Contact/Index.jsx
@@ -100,7 +100,7 @@ export default function Index({ auth, title }) {
              <section className="w-full bg-gradient-to-r from-blue-200 via-white to-blue-200 py-28">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                    <div className="text-center">
-                        <div className="inline-flex items-center justify-center space-x-2 mb-6">
+                        <div className="inline-flex items-center bg-blue-100 px-3 py-1 justify-center space-x-2 mb-6">
                             <div className="w-3 h-3 bg-blue-400 rounded-full animate-pulse"></div>
                             <span className="text-blue-300 font-medium text-sm uppercase tracking-wider"> {title}</span>
                         </div>
