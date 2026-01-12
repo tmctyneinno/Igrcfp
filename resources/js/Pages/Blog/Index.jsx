28ee2c7e76b000c91317034b711ee2d8223acb72
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-12 13:30:26 +0100

    Updated Changes

--- resources/js/Pages/Blog/Index.jsx
+++ resources/js/Pages/Blog/Index.jsx
@@ -96,7 +96,7 @@ export default function Blog({ auth, title, description }) {
             </section>
 
             {/* Newsletter Subscription Section */}
-            <section className="max-w-7xl mx-auto px-4 rounded-lg  bg-gradient-to-r from-blue-600 via-blue-00 to-green-500 ">
+            <section className="max-w-7xl mx-auto px-4 rounded-lg  bg-gradient-to-r from-blue-600 via-blue-700 to-green-500 ">
                 <div className="mx-6 md:mx-12 lg:mx-16 xl:mx-24"> {/* Increased margins */}
                     <div className="max-w-7xl mx-auto text-white text-center py-8 md:py-12 lg:py-16">
                         <h3 className="text-3xl font-semibold mb-4">Never miss an update.</h3>
