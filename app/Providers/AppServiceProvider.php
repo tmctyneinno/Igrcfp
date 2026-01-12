0bece4214a85c1fa15505ab86bd39b86d5410840
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-12 09:57:24 +0100

    Updated Changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        Inertia::share([
+        'flash' => fn () => [
+            'success' => session('success'),
+            'error'   => session('error'),
+        ],
+    ]);
+
     }
 }
