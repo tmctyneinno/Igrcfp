29c84903912350816ce5837226a2061ea906d55c
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-12 18:47:38 +0100

    Updated Changes

--- routes/admin/dashboard.php
+++ routes/admin/dashboard.php
@@ -31,4 +31,18 @@ Route::prefix('admin')->name('admin.')->middleware(['auth:admin'])->group(functi
     Route::post('/events/bulk-action', [EventController::class, 'bulkAction'])->name('events.bulk-action');
     Route::patch('/events/{event}/toggle-featured', [EventController::class, 'toggleFeatured'])->name('events.toggle-featured');
     Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
+
+     // Blog Management
+    Route::prefix('blogs')->name('blogs.')->group(function () {
+        Route::get('/', [BlogController::class, 'index'])->name('index');
+        Route::get('/create', [BlogController::class, 'create'])->name('create');
+        Route::post('/', [BlogController::class, 'store'])->name('store');
+        Route::get('/{blog}', [BlogController::class, 'show'])->name('show');
+        Route::get('/{blog}/edit', [BlogController::class, 'edit'])->name('edit');
+        Route::put('/{blog}', [BlogController::class, 'update'])->name('update');
+        Route::delete('/{blog}', [BlogController::class, 'destroy'])->name('destroy');
+        Route::patch('/{blog}/toggle-status', [BlogController::class, 'toggleStatus'])->name('toggle-status');
+        Route::post('/bulk-action', [BlogController::class, 'bulkAction'])->name('bulk-action');
+    });
+    
 });
