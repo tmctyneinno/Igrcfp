b8004667370274f0178f03cf3a3a36881dfe7881
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-13 12:12:59 +0100

    comment

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,7 +16,7 @@
         {{-- <li class="fw-medium">AI</li> --}}
     </ul>
     </div>
-
+    
     <div class="row row-cols-xxxl-5 row-cols-lg-3 row-cols-sm-2 row-cols-1 gy-4">
     <div class="col">
         <div class="card shadow-none border bg-gradient-start-1 h-100">
