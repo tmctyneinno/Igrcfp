92b48c8739209b8965659e1dc5ec5786f9ca623b
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-13 10:02:13 +0100

    Updated Changes

--- resources/views/admin/events/create.blade.php
+++ resources/views/admin/events/create.blade.php
@@ -10,7 +10,7 @@
                     <iconify-icon icon="solar:home-smile-angle-outline" class="icon text-lg"></iconify-icon>
                     Dashboard
                 </a>
-            </li>
+            </li> 
             <li>-</li>
             <li class="fw-medium">
                 <a href="{{ route('admin.events.index') }}" class="hover-text-primary">Events</a>
