d34ee3e46a514e6213dc2e342a452765785c06be
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-13 23:24:07 +0100

    Updated Changes

--- database/migrations/2026_01_13_222225_create_article_categories_table.php
+++ database/migrations/2026_01_13_222225_create_article_categories_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('article_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->integer('order')->default(0);
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
