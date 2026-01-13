dc0a3b37137229da2f0e4262441243aa5b14e986
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-13 23:54:07 +0100

    Updated Changes

--- resources/js/Pages/News/Index.jsx
+++ resources/js/Pages/News/Index.jsx
@@ -356,7 +356,7 @@ export default function News({
                   />
                   <button 
                     type="submit"
-                    className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+                    className="w-full bg-blue-900 text-white py-2 rounded-lg hover:bg-blue-700 transition"
                   >
                     Subscribe
                   </button>
