7775f6abeecb9d587c727c24eefb9f92e2afcdf4
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-13 23:36:07 +0100

    Updated Changes

--- database/migrations/2026_01_13_222144_create_articles_table.php
+++ database/migrations/2026_01_13_222144_create_articles_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
                 $table->text('excerpt')->nullable();
                 $table->longText('content'); 
                 $table->string('image_path')->nullable();
-                $table->foreignId('category_id')->constrained()->onDelete('cascade');
+                $table->foreignId('article_category_id')->constrained()->onDelete('cascade');
                 $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
                 $table->boolean('is_featured')->default(false);
                 $table->enum('status', ['draft', 'published', 'archived'])->default('draft');
