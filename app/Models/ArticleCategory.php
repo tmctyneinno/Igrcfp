4971e073fd2caa69b62554a14ef79fd28ca7fa1d
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-13 23:25:08 +0100

    Updated Changes

--- app/Models/ArticleCategory.php
+++ app/Models/ArticleCategory.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ArticleCategory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'order',
+        'is_active',
+    ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
