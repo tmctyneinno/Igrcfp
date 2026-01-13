3f5ec6f571c1836aa2f2c463fed7d5726aa70f02
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-13 10:12:13 +0100

    Updated Changes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -68,36 +68,7 @@
     <script src="{{ asset('assets/admin/js/app.js')}}"></script>
     <script src="{{ asset('assets/admin/js/homeOneChart.js')}}"></script>
     @stack('scripts')
-    @push('scripts')
-                            <script src="https://cdn.ckeditor.com/ckeditor5/41.4.2/classic/ckeditor.js"></script>
-                            <script>
-                                ClassicEditor
-                                    .create(document.querySelector('#editor'), {
-                                        // Optional: Basic toolbar configuration
-                                        toolbar: [
-                                            'heading', '|',
-                                            'bold', 'italic', 'underline', 'strikethrough', '|',
-                                            'bulletedList', 'numberedList', 'blockQuote', '|',
-                                            'link', 'imageUpload', 'insertTable', '|',
-                                            'undo', 'redo'
-                                        ]
-                                    })
-                                    .then(editor => {
-                                        // Update hidden input when editor content changes
-                                        editor.model.document.on('change:data', () => {
-                                            document.getElementById('description').value = editor.getData();
-                                        });
-                                        
-                                        // Also update on form submit
-                                        document.querySelector('form').addEventListener('submit', () => {
-                                            document.getElementById('description').value = editor.getData();
-                                        });
-                                    })
-                                    .catch(error => {
-                                        console.error(error);
-                                    });
-                            </script>
-                            @endpush
+    
 
 </body>
 </html>
