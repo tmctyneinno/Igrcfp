e06f2f015995c6b6be18ffdbf5bdd088670c6cd7
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-14 07:42:10 +0100

    Updated Changes

--- routes/admin/dashboard.php
+++ routes/admin/dashboard.php
@@ -54,6 +54,7 @@ Route::prefix('admin')->name('admin.')->middleware(['auth:admin'])->group(functi
         Route::get('/{article}', [NewsController::class, 'show'])->name('show');
         Route::get('/{article}/edit', [NewsController::class, 'edit'])->name('edit');
         Route::put('/{article}', [NewsController::class, 'update'])->name('update');
+        
         Route::put('/{article}', [NewsController::class, 'updateStatus'])->name('update-status');
         Route::delete('/{article}', [NewsController::class, 'destroy'])->name('destroy');
         Route::patch('/{article}/toggle-status', [NewsController::class, 'toggleStatus'])->name('toggle-status');
