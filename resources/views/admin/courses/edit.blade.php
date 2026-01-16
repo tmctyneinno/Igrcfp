7e4e045e914148f2f850bf79a9e5b57fc207f030
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-16 14:37:46 +0100

    Updated Changes

--- resources/views/admin/courses/edit.blade.php
+++ resources/views/admin/courses/edit.blade.php
@@ -63,7 +63,7 @@
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
-
+{{-- 
                             <div class="col-md-6">
                                 <label class="form-label">Course Code <span class="text-danger">*</span></label>
                                 <input type="text" name="code" class="form-control @error('code') is-invalid @enderror" 
@@ -71,7 +71,7 @@
                                 @error('code')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
-                            </div>
+                            </div> --}}
 
                             <div class="col-md-6">
                                 <label class="form-label">Short Title <span class="text-danger">*</span></label>
@@ -726,7 +726,7 @@ Map GRC responsibilities in your organisation.">{{ old('bulk_modules') }}</texta
                                         <button type="submit" class="btn btn-primary flex-grow-1">
                                             Update Course
                                         </button>
-                                        <a href="{{ route('admin.courses.show', $course->id) }}" class="btn btn-outline-secondary">
+                                        <a href="{{ route('admin.courses.show', $course->slug) }}" class="btn btn-outline-secondary">
                                             Cancel
                                         </a>
                                     </div>
