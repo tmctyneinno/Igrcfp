3a60e697dee4226453a17e7894ce099629f76fd7
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-16 20:55:18 +0100

    Updated Changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,7 +46,11 @@ return [
             'throw' => false,
             'report' => false,
         ],
-
+        'private' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private'),
+            'visibility' => 'private',
+        ],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
