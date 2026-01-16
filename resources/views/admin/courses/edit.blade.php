2efcae502992115fa43a77c48c0897c93c37df1c
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-16 21:09:00 +0100

    Updated Changes

--- resources/views/admin/courses/edit.blade.php
+++ resources/views/admin/courses/edit.blade.php
@@ -769,28 +769,28 @@ Map GRC responsibilities in your organisation.">{{ old('bulk_modules') }}</texta
                             <h6 class="mb-3">Banner Image</h6>
                             @if($course->banner_image) 
                                 <div id="currentBannerPreview" class="mb-3">
-    @if($course->banner_image)
-        @php
-            $fullPath = storage_path('app/private/' . $course->banner_image);
-            $exists = file_exists($fullPath);
-        @endphp
-        
-        @if($exists)
-            @php
-                $imageData = base64_encode(file_get_contents($fullPath));
-                $src = 'data:image/jpeg;base64,' . $imageData;
-            @endphp
-            <img src="{{ $src }}" 
-                 alt="Current banner image" 
-                 class="img-fluid rounded-8 border" 
-                 style="max-height: 100px;">
-        @else
-            <div class="alert alert-warning p-2">
-                File not found: {{ $course->banner_image }}
-            </div>
-        @endif
-    @endif
-</div>
+                                    @if($course->banner_image)
+                                        @php
+                                            $fullPath = storage_path('app/private/' . $course->banner_image);
+                                            $exists = file_exists($fullPath);
+                                        @endphp
+                                        
+                                        @if($exists)
+                                            @php
+                                                $imageData = base64_encode(file_get_contents($fullPath));
+                                                $src = 'data:image/jpeg;base64,' . $imageData;
+                                            @endphp
+                                            <img src="{{ $src }}" 
+                                                alt="Current banner image" 
+                                                class="img-fluid rounded-8 border" 
+                                                style="max-height: 100px;">
+                                        @else
+                                            <div class="alert alert-warning p-2">
+                                                File not found: {{ $course->banner_image }}
+                                            </div>
+                                        @endif
+                                    @endif
+                                </div>
                                 <small class="text-muted d-block mb-3">Current banner</small>
                             @else
                                 <div id="noBannerImagePlaceholder" class="text-muted py-2">
