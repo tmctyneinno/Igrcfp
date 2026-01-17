e1c3f703e354da4567f379f7e5a339277be84d13
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-17 07:51:54 +0100

    Updated Changes

--- resources/views/admin/courses/create.blade.php
+++ resources/views/admin/courses/create.blade.php
@@ -502,7 +502,7 @@
                             <div class="col-12">
                                 <label class="form-label">Meta Description</label>
                                 <textarea name="meta_description" id="metaDescription" class="form-control @error('meta_description') is-invalid @enderror" 
-                                          rows="3" placeholder="Brief description for search engines (max 160 characters)" maxlength="160">{{ old('meta_description') }}</textarea>
+                                          rows="3" placeholder="Brief description for search engines (max 160 characters)" maxlength="180">{{ old('meta_description') }}</textarea>
                                 <div class="d-flex justify-content-between mt-1">
                                     <small class="text-muted">Recommended: 150-160 characters</small>
                                     <small class="character-count" data-target="meta_description">0/160</small>
