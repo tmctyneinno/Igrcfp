183395b21de19f35ec800cda84fbe846c79be922
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-17 10:22:07 +0100

    Updated Changes

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CoursMaterial;
 use App\Models\Course;
 use App\Models\CourseModule;
 use App\Models\CourseMaterial;
