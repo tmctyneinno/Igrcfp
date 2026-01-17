b7cf60d612771aded96f5b3a93c43473b1f81123
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-17 10:21:32 +0100

    Updated Changes

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use DB;
 
 class CourseController extends Controller
 {
