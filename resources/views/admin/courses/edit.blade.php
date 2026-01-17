52da81837f0f75bc81a6452fc26005c249bb5456
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-17 09:35:56 +0100

    Updated Changes

--- resources/views/admin/courses/edit.blade.php
+++ resources/views/admin/courses/edit.blade.php
@@ -416,7 +416,7 @@ Final examination">{{ old('assessment_structure', $course->assessment_structure)
                             <div class="col-12">
                                 <label class="form-label">Code of Professional Conduct (one per line)</label>
                                 <textarea id="code_of_conduct" name="code_of_conduct" class="form-control @error('code_of_conduct') is-invalid @enderror" rows="4" 
-                                          >{!! old('code_of_conduct', $course->code_of_conduct) !!}</textarea>
+                                          >{!!  $course->code_of_conduct !!}</textarea>
                                 @error('code_of_conduct')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
