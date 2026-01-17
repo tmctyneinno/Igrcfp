d04fb575a3562aa47eaa0c04e78bf1874290ebd8
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-17 10:24:03 +0100

    Updated Changes

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Course;
+use App\Models\ActivityLog;
 use App\Models\CourseModule;
 use App\Models\CourseMaterial;
 use Illuminate\Http\Request;
