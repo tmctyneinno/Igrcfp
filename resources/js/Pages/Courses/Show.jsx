a97d77fa80de0db04454ed68d94a320afb465001
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-23 20:00:51 +0100

    Updated Changes

--- resources/js/Pages/Courses/Show.jsx
+++ resources/js/Pages/Courses/Show.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Head, Link } from '@inertiajs/react';
 import { motion } from 'framer-motion';
 import GuestLayout from '@/Layouts/GuestLayout';
-import { useEnrollment } from '@/Components/Courses/EnrollmentContext';
+import { useEnrollment } from '@/Contexts/EnrollmentContext';
 import { 
   CheckCircleIcon, 
   ClockIcon, 
