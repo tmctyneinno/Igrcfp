d3dc5f98dc290a5fdba988ccabc3c037666dcaf0
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-23 18:22:47 +0100

    Updated Changes

--- resources/js/Pages/Courses/Show.jsx
+++ resources/js/Pages/Courses/Show.jsx
@@ -198,7 +198,7 @@ export default function CourseShow({ auth, course }) {
                     className={`
                         py-4 px-1 border-b-2 font-medium text-sm flex items-center whitespace-nowrap
                         ${activeTab === tab.id
-                        ? 'border-blue-600 text-blue-600'
+                        ? 'border-blue-900 text-blue-600'
                         : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                         }
                     `}
