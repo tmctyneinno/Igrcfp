08521054029d34c5fc104d9a76a326ad8a2edfab
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-23 18:55:47 +0100

    Updated Changes

--- resources/js/Pages/Courses/Show.jsx
+++ resources/js/Pages/Courses/Show.jsx
@@ -231,10 +231,10 @@ export default function CourseShow({ auth, course }) {
                 >
                     <div className="prose prose-lg max-w-none">
                     {course.full_description ? (
-                        //  <pre className="text-xs whitespace-pre-wrap">
-                        //     {JSON.stringify(course.full_description?.substring(0, 500) + '...', null, 2)}
-                        // </pre>
-                         <div dangerouslySetInnerHTML={{ __html: formatTextWithParagraphs(course.full_description) }} />
+                        <div className="prose prose-lg prose-headings:text-gray-900 prose-p:text-gray-700 prose-strong:text-gray-900 prose-li:text-gray-700 max-w-none">
+                        <div dangerouslySetInnerHTML={{ __html: course.full_description }} />
+                        </div>
+                        //  <div dangerouslySetInnerHTML={{ __html: formatTextWithParagraphs(course.full_description) }} />
                     ) : (
                         <div className="text-gray-600">
                         <p className="text-lg mb-6">This comprehensive certification programme is designed to provide you with the essential skills and knowledge needed to excel in your professional field.</p>
