b001fa8ffefbf085c0f4ab0280d08c4c01a721a0
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-23 16:53:47 +0100

    Updated Changes

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -59,46 +59,6 @@ class CourseController extends Controller
     {
         $course = Course::with([ 'modules'])->where('slug', $slug)->firstOrFail();
 
-        return Inertia::render('Courses/Show', [
-            'course' => [
-                'id' => $course->id,
-                'title' => $course->title,
-                'description' => $course->description,
-                'excerpt' => $course->excerpt,
-                'level' => $course->level,
-                'level_badge' => $course->level_badge,
-                'price' => $course->current_price,
-                'has_discount' => $course->has_discount,
-                'discount_price' => $course->discount_price,
-                'discount_percentage' => $course->discount_percentage,
-                'image_url' => $course->image_url,
-                'video_url' => $course->video_url,
-                'video_embed_code' => $course->video_embed_code,
-                'instructor' => $course->instructor
-                    ? [
-                        'id' => $course->instructor->id,
-                        'name' => $course->instructor->name,
-                    ]
-                    : null,
-                'rating' => round($course->averageRating(), 1),
-                'reviews_count' => $course->reviewsCount(),
-                'enrollments_count' => $course->activeEnrollmentsCount,
-                'modules' => $course->modules()->get()->map(function($module) {
-                    return [
-                        'id' => $module->id,
-                        'title' => $module->title,
-                        'order' => $module->order,
-                        'lessons_count' => $module->lessons()->count(),
-                    ];
-                }),
-                'lessons' => $course->lessons()->get()->map(function($lesson) {
-                    return [
-                        'id' => $lesson->id,
-                        'title' => $lesson->title,
-                        'duration' => $lesson->duration,
-                    ];
-                }),
-            ]
-        ]);
+        return Inertia::render('Courses/Show');
     }
 }
