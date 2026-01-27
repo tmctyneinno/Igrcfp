b06fad99bb7ba157c86eec733006fd49ab54505c
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-27 11:59:33 +0100

    Updated Changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -207,10 +207,7 @@ class HomeController extends Controller
     }
 
     public function privacyPreferenceCenter(){
-        return Inertia::render('TermsCondition/Index', [
-            'title' => 'News',
-            'description' => 'Learn about the  Institute of Governance, Risk & Compliance & Financial Crime Prevention (IGRCFP)  Professionals body.',
-        ]);
+        return Inertia::render('PrivacyPreferenceCenter/Index');
     }
    
 }
