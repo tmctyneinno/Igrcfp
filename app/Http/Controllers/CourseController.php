ee4280c2841c3a278f40eb572b73245aaf1fe804
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-27 09:40:32 +0100

    Updated Changes

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -131,7 +131,7 @@ class CourseController extends Controller
             'slug' => $slug,
             'controller' => __METHOD__,
         ]);
-        return Inertia::render('Courses/show', [
+        return Inertia::render('Courses/Show', [
             'course' => $formattedCourse,
             'auth' => Auth::guard('web')->user(),
             'isEnrolled' => $isEnrolled,
