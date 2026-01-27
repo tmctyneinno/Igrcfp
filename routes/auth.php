0d97deafe96c6d063fbb8e44524ac4aafd97e415
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-27 13:45:51 +0100

    Updated Changes

--- routes/auth.php
+++ routes/auth.php
@@ -34,6 +34,9 @@ Route::middleware('guest')->group(function () {
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
 });
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [AuthenticatedSessionController::class, 'logout'])
+        ->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
