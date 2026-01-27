6e37c40cdbd50a7aac45fd0571ffe2b975d15818
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-27 12:04:33 +0100

    Updated Changes

--- resources/js/Layouts/Footer.jsx
+++ resources/js/Layouts/Footer.jsx
@@ -272,7 +272,7 @@ export default function Footer() {
                     
                     <div className="flex items-center gap-2">
                         
-                        <div className="flex gap-4">
+                        <div className="flex gap-2">
                             <Link href="/privacy-policy" className="text-sm text-gray-400 hover:text-white transition">
                                 Privacy Policy
                             </Link>
