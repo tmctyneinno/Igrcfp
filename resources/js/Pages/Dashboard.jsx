d9d165363c353b79792d72bd870146adf6c39ac6
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-27 11:06:33 +0100

    Updated Changes

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,10 +1,16 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
-export default function PrivacyPolicy() {
+export default function Dashboard() {
     return (
-        <GuestLayout auth={auth}>
-                    <Head title='Privacy Policy' />
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Dashboard
+                </h2>
+            }
+        >
+            <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -15,6 +21,6 @@ export default function PrivacyPolicy() {
                     </div>
                 </div>
             </div>
-        </GuestLayout>
+        </AuthenticatedLayout>
     );
 }
