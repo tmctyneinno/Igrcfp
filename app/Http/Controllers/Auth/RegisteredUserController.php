b2e2542c0e8d097a982761acc04cb77939eb948d
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-27 14:35:51 +0100

    Updated Changes

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,6 +31,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
+            'role' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             // 'linkedin_url' => [
@@ -44,6 +45,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'role' => $request->role,
             'linkedin_url' =>  $request->linkedin_url,
             'password' => Hash::make($request->password),
             'email_verification_token' => Str::random(60),
