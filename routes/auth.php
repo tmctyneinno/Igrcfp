fac14e23a9232f33e8f4c790a03fb4d68c3e14a5
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-27 13:49:51 +0100

    Updated Changes

--- routes/auth.php
+++ routes/auth.php
@@ -34,7 +34,7 @@ Route::middleware('guest')->group(function () {
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
 });
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'logout'])
         ->name('logout');
     
--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,7 @@ Route::get('/courses/{slug}', [CourseController::class, 'show'])->name('courses.
 
 
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
