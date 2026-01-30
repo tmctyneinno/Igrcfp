b938f72e65acf01e51637cc0743773019121c97f
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-30 19:39:16 +0100

    Updated Changes

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
+                    IGRFCP
                 </h2>
             }
         >
