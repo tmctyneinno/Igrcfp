5f6f46250ee3039977350c182a0aa731245e9e18
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-30 20:01:15 +0100

    Updated Changes

---
+++ routes/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+Route::middleware(['auth', 'verified'])->group(function () {        
+    
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+});
+
+
