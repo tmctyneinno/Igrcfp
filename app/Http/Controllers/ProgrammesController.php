c750794cdffdc683feb03cd78f83b54054652d53
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-03 11:35:56 +0100

    Updated Changes

---
+++ app/Http/Controllers/ProgrammesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+
+class ProgrammesController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Programmes/Index');
+    }
+
+    public function grc()
+    {
+        return Inertia::render('Programmes/GRC');
+    }
+
+    public function financialCrime()
+    {
+        return Inertia::render('Programmes/FinancialCrime');
+    }
+
+    public function crypto()
+    {
+        return Inertia::render('Programmes/Crypto');
+    }
+
+    public function cybersecurity()
+    {
+        return Inertia::render('Programmes/Cybersecurity');
+    }
+
+    public function ai()
+    {
+        return Inertia::render('Programmes/AI');
+    }
+
+    public function allCourses()
+    {
+        return Inertia::render('Programmes/AllCourses');
+    }
+}
