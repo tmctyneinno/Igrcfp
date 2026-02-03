8587b52e1820395af99e14b59aca97a497614bf5
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-03 14:45:31 +0100

    Updated Changes

--- resources/js/Layouts/NavBar.jsx
+++ resources/js/Layouts/NavBar.jsx
@@ -185,10 +185,12 @@ export default function NavBar({ auth }) {
                                 
                                 {/* AI & Emerging Technology */}
                                 <div className="px-4 py-3 hover:bg-blue-50 transition duration-200">
+                                    <Link href="programmes/ai">
                                     <div className="flex items-center">
                                         <div className="w-2 h-2 bg-yellow-600 rounded-full mr-3"></div>
                                         <h4 className="font-semibold text-gray-800">AI & Emerging Technology</h4>
                                     </div>
+                                    </Link>
                                     <div className="ml-5 mt-2 space-y-1">
                                         <Link href="#" className="block text-sm text-gray-600 hover:text-blue-900">
                                             AI Governance & Ethics
