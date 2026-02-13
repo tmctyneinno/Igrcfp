235135f2aca3d2476ec8d74381db4a3b8a5dac8b
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 16:56:57 +0100

    Updated Changes

--- routes/web.php
+++ routes/web.php
@@ -49,10 +49,6 @@ Route::post('/courses/{course:slug}/enroll', [CourseController::class, 'processE
 
 
 
-
-
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
