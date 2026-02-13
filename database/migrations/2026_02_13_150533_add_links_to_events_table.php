b244a5d58493424e712d659467cf11c889abb4f6
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 16:07:57 +0100

    Updated Changes

--- database/migrations/2026_02_13_150533_add_links_to_events_table.php
+++ database/migrations/2026_02_13_150533_add_links_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->string('meeting_link')->nullable()->after('registration_link');
+            $table->string('meeting_link')->nullable()->after('capacity');
         });
     }
 
