8b303170340456843fc1889d0e28a905dbff2b6d
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 17:21:58 +0100

    Updated Changes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    Route::get('/cart/count', [CartController::class, 'count']);
     return $request->user();
 });
