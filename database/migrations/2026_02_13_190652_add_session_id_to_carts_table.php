f30f45b0d5c9056dd5ea8eb060e5b41313a73a26
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 20:07:59 +0100

    Updated Changes

--- database/migrations/2026_02_13_190652_add_session_id_to_carts_table.php
+++ database/migrations/2026_02_13_190652_add_session_id_to_carts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('carts', function (Blueprint $table) {
-            //
+            $table->string('session_id')->nullable()->after('user_id');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('carts', function (Blueprint $table) {
-            //
+            $table->dropColumn('session_id');
         });
     }
 };
