3795c7b9efb74e171e0210dc1ff31e9aae80e15a
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 17:42:03 +0100

    Updated Changes

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -9,7 +9,7 @@ use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->group(function () {        
     
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
     Route::get('/my-courses', [DashboardController::class, 'myCourse'])->name('my-courses');
     Route::get('/notifications/index', [DashboardController::class, 'notifications'])->name('notifications.index');
