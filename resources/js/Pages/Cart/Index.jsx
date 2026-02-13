20424376cdf38e99e42d95a9e478be3fc3728b5e
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 17:06:58 +0100

    Updated Changes

--- resources/js/Pages/Cart/Index.jsx
+++ resources/js/Pages/Cart/Index.jsx
@@ -94,7 +94,7 @@ export default function Cart({ cart }) {
                                 </div>
                                 
                                 <Link
-                                    href={route('checkout')}
+                                    href={route('checkout.index')}
                                     className="block w-full text-center py-3 px-4 bg-gradient-to-r from-blue-900 to-indigo-900 text-white font-semibold rounded-lg hover:from-blue-700 hover:to-indigo-700 transition-all duration-300"
                                 >
                                     Proceed to Checkout
