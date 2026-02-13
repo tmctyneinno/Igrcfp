bdd903f4826facd5527057bb64fa73c7049e5a16
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 17:07:58 +0100

    Updated Changes

--- resources/js/Pages/Cart/Index.jsx
+++ resources/js/Pages/Cart/Index.jsx
@@ -10,7 +10,7 @@ export default function Cart({ cart }) {
     };
 
     return (
-        <GuestLayout>
+        <GuestLayout> 
             <Head title="Shopping Cart" />
             
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
