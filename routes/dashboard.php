01c98ec23b4e5f49a84748d52a34d9b7f962ca23
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 19:07:58 +0100

    Updated Changes

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -20,7 +20,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/courses/{course:slug}', [DashboardController::class, 'courseSlug'])->name('courses.show');
     // Cart routes
     Route::get('/cart/index', [CartController::class, 'index'])->name('dashboard.cart.index');
-    Route::post('/cart/add/{course}', [CartController::class, 'add'])->name('dashboard.cart.add');
+    // Route::post('/cart/add/{course}', [CartController::class, 'add'])->name('dashboard.cart.add');
+    Route::post('/dashboard/cart/add/{course}', [CartController::class, 'add'])->name('dashboard.cart.add'); 
     Route::delete('/cart/remove/{item}', [CartController::class, 'remove'])->name('cart.remove');
     Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
 
