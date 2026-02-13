7a07d953984d035e5ed8061914b7ac69a7a72053
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 12:49:36 +0100

    Updated Changes

--- resources/js/Pages/Courses/Index.jsx
+++ resources/js/Pages/Courses/Index.jsx
@@ -3,9 +3,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link, usePage, router } from '@inertiajs/react';
 import { motion, AnimatePresence } from 'framer-motion';
-import CourseCard from '@/Components/Courses/c';
-import FilterSidebar from '@/components/FilterSidebar';
-import SearchBar from '@/components/SearchBar';
+import CourseCard from '@/components/Courses/CourseCard';
+import FilterSidebar from '@/components/Courses/FilterSidebar';
+import SearchBar from '@/components/Courses/SearchBar';
 
 export default function Courses({ courses, filters, filterOptions }) {
     const { url } = usePage();
