f8f76211aa505fafdb591ea6953262023551c465
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 20:23:59 +0100

    Updated Changes

--- resources/js/Pages/Checkout/Index.jsx
+++ resources/js/Pages/Checkout/Index.jsx
@@ -169,7 +169,7 @@ export default function Checkout({ cart, user }) {
                             </div>
 
                             <Link
-                                href={route('cart.index')}
+                                href={route('dashboard.cart.index')}
                                 className="block w-full text-center py-2 px-4 text-blue-900 hover:text-blue-700 font-semibold"
                             >
                                 Edit Cart
