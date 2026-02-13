84c8f99ada8eea486acde9a3b45199ef76844ae8
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-13 19:56:58 +0100

    Updated Changes

--- database/migrations/2026_02_13_185553_add_status_to_carts_table.php
+++ database/migrations/2026_02_13_185553_add_status_to_carts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('carts', function (Blueprint $table) {
-            //
+            $table->string('status')->default('active')->after('user_id');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('carts', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 };
