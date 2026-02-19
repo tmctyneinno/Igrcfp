796559813f1dd95398400948ceed1390694cb621
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-20 00:59:00 +0100

    Updated Changes

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -278,7 +278,7 @@ class CourseController extends Controller
             if ($request->filled('target_audience')) {
                 // $lines = explode("\n", $request->target_audience);
                 // $validated['target_audience'] = array_map('trim', array_filter($lines));
-                $validated['target_audience'] = $request->target_audience);
+                $validated['target_audience'] = $request->target_audience;
             }
 
             // Update course
