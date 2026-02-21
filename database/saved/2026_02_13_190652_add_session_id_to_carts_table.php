f81217886baa3a131fc2973121c7bb909f2ef3c1
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-21 06:38:27 +0100

    comment

--- database/saved/2026_02_13_190652_add_session_id_to_carts_table.php
+++ database/saved/2026_02_13_190652_add_session_id_to_carts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->string('session_id')->nullable()->after('user_id');
         });
     }
-
+ 
     /**
      * Reverse the migrations.
      */
