583e675e09d4b6b1e9b78e8068429e55514c170c
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-27 15:07:02 +0100

    Updated Changes

--- database/migrations/2026_01_11_191733_create_events_registrations_table.php
+++ database/migrations/2026_01_11_191733_create_events_registrations_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    { 
         Schema::create('events_registrations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('event_id')->constrained()->onDelete('cascade');
