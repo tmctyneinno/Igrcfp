9cf801ff1692fb60f3d24cc4920f90d072e0fa7e
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-27 14:55:03 +0100

    Updated Changes

--- app/Http/Controllers/Admin/LessonController.php
+++ app/Http/Controllers/Admin/LessonController.php
@@ -18,7 +18,7 @@ class LessonController extends Controller
         return view('admin.courses.modules.lessons.index', compact('course', 'module', 'lessons'));
     }
 
-    public function create(Course $course, CourseModule $module)
+    public function create( $course,  $module)
     {
         return view('admin.courses.modules.lessons.create', compact('course', 'module'));
     }
