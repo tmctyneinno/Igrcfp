08f24f5447e2a105fe0a0e21888cf038b29be9c8
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-27 14:17:02 +0100

    Updated Changes

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -11,7 +11,7 @@ class Module extends Model
     use HasFactory;
 
     protected $fillable = [
-        'course_id',
+        'course_id', 
         'title',
         'slug',
         'description',
