991e81444c54254d62602e33a4b16c6e64864c34
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-27 13:23:02 +0100

    Updated Changes

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -278,14 +278,14 @@ export default function AuthenticatedLayout({ header, children }) {
                     </div>
                 </div>
             </nav>
-
+ 
             {header && (
                 <header className="bg-white shadow">
                     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                         {header}
                     </div>
                 </header>
-            )}
+            )} 
 
             <main className="py-8">
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
