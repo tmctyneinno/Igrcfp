e6ff37734b3ba6c61d71da632ec93b7de80199d7
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-27 13:28:02 +0100

    Updated Changes

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,6 +5,7 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link, usePage, router } from '@inertiajs/react';
 import React, { useState, useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
+import {AuthenticatedFooter} from '@/Layouts/AuthenticatedFooter'
 import { 
   BellIcon, 
   ShoppingCartIcon, 
@@ -292,6 +293,7 @@ export default function AuthenticatedLayout({ header, children }) {
                     {children}
                 </div>
             </main>
+            <AuthenticatedFooter/>
         </div>
     );
 }
