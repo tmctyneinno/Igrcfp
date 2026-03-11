fdf47c2e3a37145a4a11b986187fba6b017a64e9
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-03-11 15:09:27 +0100

    Updated Changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -150,7 +150,14 @@ class HomeController extends Controller
             'title' => 'Certification Pathway',
             'description' => '',
         ]);
-    }
+    }cgfcsSpecialist
+
+    public function certificationsPathway(){
+        return Inertia::render('Certifications/Pathway', [
+            'title' => 'Certification Pathway',
+            'description' => '',
+        ]);
+    }cgfcsSpecialist
 
     public function eventsIndex(){
         $currentDate = now()->toDateString();
