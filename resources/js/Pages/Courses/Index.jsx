7a885140e03c30721914569d793f47bbf845a923
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-03-11 16:15:26 +0100

    Updated Changes

--- resources/js/Pages/Courses/Index.jsx
+++ resources/js/Pages/Courses/Index.jsx
@@ -265,7 +265,7 @@ export default function Courses({ auth, courses, filters, filterOptions }) {
                                     showFilters 
                                         ? 'grid-cols-1 md:grid-cols-2 xl:grid-cols-3' 
                                         : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
-                                }`}>
+                                }`}> 
                                     {courses.data.map((course, index) => (
                                         <motion.div
                                             key={course.id}
