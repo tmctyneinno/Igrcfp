26df66c2c6b612e5466e31485c092888ddf4bf99
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-03-11 15:16:26 +0100

    Updated Changes

--- resources/js/Layouts/NavBar.jsx
+++ resources/js/Layouts/NavBar.jsx
@@ -188,13 +188,6 @@ export default function NavBar({ auth }) {
                                     </Link>
                                 </div>
                                 <div className="px-4 py-3 hover:bg-blue-50 transition duration-200">
-                                    <Link href={route('certifications.pathway')}>
-                                        <div className="flex items-center">
-                                            <h4 className="font-semibold text-gray-800">Certification Pathway</h4>
-                                        </div>
-                                    </Link>
-                                </div>
-                                <div className="px-4 py-3 hover:bg-blue-50 transition duration-200">
                                     <Link href={route('cgfcs.specialist')}>
                                         <div className="flex items-center">
                                             <h4 className="font-semibold text-gray-800">Certified GRC & Financial Crime Specialist (CGFCS)</h4>
