43e603e09b5a3b1a1252fc536a1b99e7572b3683
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-03-13 18:05:18 +0100

    Updated Changes

--- app/Http/Controllers/Admin/AssessmentController.php
+++ app/Http/Controllers/Admin/AssessmentController.php
@@ -182,14 +182,7 @@ class AssessmentController extends Controller
     public function store(Request $request)
     {
         // Debug: Log everything
-        \Log::info('=== ASSESSMENT STORE DEBUG ===');
-        \Log::info('All request data:', $request->all());
-        \Log::info('Request method: ' . $request->method());
-        \Log::info('Request URL: ' . $request->fullUrl());
-        \Log::info('Has title? ' . ($request->has('title') ? 'Yes' : 'No'));
-        \Log::info('Title value: ' . $request->input('title', 'NOT FOUND'));
-        \Log::info('Has course_id? ' . ($request->has('course_id') ? 'Yes' : 'No'));
-        \Log::info('course_id value: ' . $request->input('course_id', 'NOT FOUND'));
+       
         // Check if the request is hitting this method at all
         if (!$request->isMethod('post')) {
             \Log::error('Not a POST request!');
