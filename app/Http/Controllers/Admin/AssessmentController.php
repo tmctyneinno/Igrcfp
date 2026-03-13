106619a3678386a981863ea3339ebb451db3c8b4
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-03-13 18:16:17 +0100

    Updated Changes

--- app/Http/Controllers/Admin/AssessmentController.php
+++ app/Http/Controllers/Admin/AssessmentController.php
@@ -239,7 +239,7 @@ class AssessmentController extends Controller
 
        
 
-            // $message = $this->getSuccessMessage($request->assessment_level);
+            // $message = $this->getSuccessMessage('quiz');
             return redirect()->route('admin.assessments.course', $assessment->course_id)
                 ->with('success', $message);
 
