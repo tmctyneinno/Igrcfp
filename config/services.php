038cd2d67712193cc57d75dddd08cae5606a24fe
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-03-24 13:35:14 +0100

    Updated Changes

--- config/services.php
+++ config/services.php
@@ -28,12 +28,10 @@ return [
         'key' => env('RESEND_KEY'),
     ],
     'recaptcha' => [
-        
-        'site_key' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-        'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+        'site_key' => env('RECAPTCHA_SITE_KEY'),
+        'secret_key' => env('RECAPTCHA_SECRET_KEY'),
        
     ],
-
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
