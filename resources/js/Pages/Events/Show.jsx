5c2e99d0c7fc44f5011a9ba4c50e79f9b5f94cb3
Daniel Eshanokpe <eshanokpe@gmail.com>
2026-04-16 18:08:13 +0100

    igrcfp_course

--- resources/js/Pages/Events/Show.jsx
+++ resources/js/Pages/Events/Show.jsx
@@ -297,9 +297,9 @@ export default function EventShow({ auth, event, relatedEvents }) {
                                                 Join Meeting
                                             </span>
                                         </a>
-                                    ) : (
+                                    ) : ( 
                                         <Link
-                                            href={route('events.register', event.li)}
+                                            href={route('events.register', event.slug)}
                                             className="block w-full text-center py-3 px-4 bg-gradient-to-r from-blue-900 to-indigo-900 text-white font-semibold rounded-lg hover:from-blue-700 hover:to-indigo-700 transition-all duration-300 shadow-lg hover:shadow-xl"
                                             preserveState
                                             preserveScroll
