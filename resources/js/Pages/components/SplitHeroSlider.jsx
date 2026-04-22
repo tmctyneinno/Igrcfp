d018665cf594a36564936dd3b6d181a701b25ed6
Infoscert <infoscert@Infoscerts-MacBook-Pro.local>
2026-04-22 13:40:44 +0100

    Update Welcome.jsx, Update SplitHeroSlider.jsx

--- resources/js/Pages/components/SplitHeroSlider.jsx
+++ resources/js/Pages/components/SplitHeroSlider.jsx
@@ -9,7 +9,7 @@ export default function SplitHeroSlider({ auth }) {
         {
             id: 1,
             title: "Empower Professionals, Strengthening Governance, Preventing Financial Crime",
-            highlighted: "Tutors",
+            highlighted: "",
             description: "The Institute of GRC & Financial Crime Prevention (IGRCFP) is a global professional body dedicated to advancing excellence in governance, risk management, compliance, and financial crime prevention.",
             image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200&q=80",
             ctaPrimary: "Join the Institute",
