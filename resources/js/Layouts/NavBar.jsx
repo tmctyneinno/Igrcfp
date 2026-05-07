9e97016e663096575558c73bd7b0d82ab8082c12
Infoscert <eshanokpe@gmail.com>
2026-05-07 13:16:44 +0100

    feat: implement NavBar

--- resources/js/Layouts/NavBar.jsx
+++ resources/js/Layouts/NavBar.jsx
@@ -63,7 +63,7 @@ export default function NavBar({ auth }) {
                         {/* Home */}
                         <Link 
                             href="/" 
-                            className="px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-900 hover:bg-blue-50 rounded-lg transition duration-200"
+                            className="px-3 py-2 text-md font-medium text-gray-700 hover:text-blue-900 hover:bg-blue-50 rounded-lg transition duration-200"
                         >
                             Home
                         </Link>
