9c5223b1494d1afecf04b0b07f0593926a9eae35
infoscert <eshanokpe@gmail.com>
2026-05-16 10:30:22 +0100

    fix: make improvements

---
+++ public/build/assets/Index copy-CtE22-wA.js
@@ -0,0 +1,1 @@
+import{j as t,H as r}from"./app-Da9fB_5C.js";import{A as o}from"./AuthenticatedLayout-C5AGsaKx.js";import"./transition-B1zGYH6L.js";import"./BellIcon-DPp4b48B.js";import"./ChevronDownIcon-B02p6Z5i.js";import"./ArrowRightOnRectangleIcon-BJRIaJm6.js";function d(){return t.jsx(o,{children:t.jsx(r,{title:"Dashboard"})})}export{d as default};
