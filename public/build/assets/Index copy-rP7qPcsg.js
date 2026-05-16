d99175753fe53c2bdebae2b8efbbe3630b43bcba
infoscert <eshanokpe@gmail.com>
2026-05-16 11:47:47 +0100

    refactor: make improvements

---
+++ public/build/assets/Index copy-rP7qPcsg.js
@@ -0,0 +1,1 @@
+import{j as t,H as r}from"./app-DWkj0c1t.js";import{A as o}from"./AuthenticatedLayout-CKfPL5_W.js";import"./transition-5Kfrg5dP.js";import"./BellIcon-BQc2uToE.js";import"./ChevronDownIcon-DnAqbm6h.js";import"./ArrowRightOnRectangleIcon-BI-YAAeY.js";function d(){return t.jsx(o,{children:t.jsx(r,{title:"Dashboard"})})}export{d as default};
