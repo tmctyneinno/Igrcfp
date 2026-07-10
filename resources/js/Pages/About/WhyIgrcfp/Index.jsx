db3e10f07d8ce3277b72eef2a2012df7c204e83b
Daniel Eshanokpe <eshanokpe@gmail.com>
2026-07-10 10:09:25 -0700

    update

--- resources/js/Pages/About/WhyIgrcfp/Index.jsx
+++ resources/js/Pages/About/WhyIgrcfp/Index.jsx
@@ -12,7 +12,7 @@ export default function Index({ auth, title, description }) {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                    <div className="text-center">
                         <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-6">
-                           Why IGRCFP
+                           Why IGRCFP  
                         </h1>
                         <p className="text-xl text-gray-600 max-w-3xl mx-auto">
                             The Institute of Governance, Risk, Compliance & Financial Crime Prevention 
